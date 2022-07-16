a678718c9bfa1bbbe912f67c10fc2adcc5ea258a
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-17 00:49:58 +0530

    ENV FILE

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "1da9585438msh6663a66d42828a2p1d79e5jsn15788eb99dd3",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    "X-RapidAPI-Host": process.env.RAPID_API_HOST
   }
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "9e9f39e7445e6c7cfb862f79e5577410";
+export const GEO_API_URL = process.env.GEO_API_URL;
+export const WEATHER_API_URL = process.env.WEATHER_API_URL;
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
