f0c19e7d4360bceecbd262863bef00a7e041acbe
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-17 00:22:33 +0530

    Forecast Progress

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -1,11 +1,55 @@
-import { Accordion } from "react-accessible-accordion";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel
+} from "react-accessible-accordion";
 
-const Forecast = ({data}) => {
+const Forecast = ({ data }: { data: any }) => {
+  const WEEK_DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
   return (
     <div>
       <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
-        
+        {data.list.splice(0, 7).map((item: any, idx: number) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item?.weather[0]?.icon}.png`}
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">
+                    {item?.weather[0]?.description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item?.main?.temp_min)}°C /{" "}
+                    {Math.round(item?.main?.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
       </Accordion>
     </div>
   );
