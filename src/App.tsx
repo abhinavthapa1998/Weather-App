95f369a502d6c81111707f0e228386d2ad10ae32
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-16 23:53:27 +0530

    CurrentWeather Widget Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import "./styles.css";
 import Search from "./components/Search/Search";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
+import { useState } from "react";
 export default function App() {
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [forecast, setForecast] = useState({});
+
   const handleOnSearchChange = (searchData: any) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   };
+
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- src/api.ts
+++ src/api.ts
@@ -7,3 +7,5 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9e9f39e7445e6c7cfb862f79e5577410";
