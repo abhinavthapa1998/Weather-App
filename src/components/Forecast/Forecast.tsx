5db04694a857d406b2b666ced202f014d2660b89
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-17 00:33:03 +0530

    Forecast Grid

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -5,6 +5,7 @@ import {
   AccordionItemHeading,
   AccordionItemPanel
 } from "react-accessible-accordion";
+import "./Forecast.css";
 
 const Forecast = ({ data }: { data: any }) => {
   const WEEK_DAYS = [
@@ -41,13 +42,40 @@ const Forecast = ({ data }: { data: any }) => {
                     {item?.weather[0]?.description}
                   </label>
                   <label className="min-max">
-                    {Math.round(item?.main?.temp_min)}°C /{" "}
+                    {Math.round(item?.main?.temp_min)}°C /
                     {Math.round(item?.main?.temp_max)}°C
                   </label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure</label>
+                  <label>{item?.main?.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity</label>
+                  <label>{item?.main?.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds</label>
+                  <label>{item?.clouds?.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed</label>
+                  <label>{item?.wind?.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea Level</label>
+                  <label>{item?.main?.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels like:</label>
+                  <label>{Math.round(item?.main?.feels_like)}°C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
